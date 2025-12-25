4d154cc44fd671d6b387e2430fea6305e7c2606e
Leo <leooo0s2@icloud.com>
2025-12-25 13:18:32 -0300

    Dados no corpo da requisição

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,18 @@ import express from "express";
 const PORT = 3333;
 
 const app = express();
+app.use(express.json());
 
 app.get("/products", (req, res) => {
   const { page, limit } = req.query;
 
   res.send(`pagina ${page} de ${limit}`);
+});
+
+app.post("/products", (req, res) => {
+  const { name, price } = req.body;
+
+  res.send(`Produto ${name} custa ${price}`);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
