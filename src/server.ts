fcde180cd98bec6463fd92a78fba74c40f321f68
Leo <leooo0s2@icloud.com>
2025-12-25 12:58:50 -0300

    Enviando requisição com o método GET

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,8 @@ const PORT = 3333;
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World Express!");
+});
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
