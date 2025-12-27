0485818363e4890fb6071ea6d64435217c6bfa01
Leo <leooo0s2@icloud.com>
2025-12-27 10:15:14 -0300

    AppError

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { AppError } from "../utils/AppError";
 
 class ProductsController {
   index(req: Request, res: Response) {
@@ -10,7 +11,8 @@ class ProductsController {
   create(req: Request, res: Response) {
     const { name, price } = req.body;
 
-    throw new Error("ERRO DE EXEMPLO!");
+    // throw new Error("ERRO DE EXEMPLO!");
+    // throw new AppError("ERRO DE EXEMPLO!");
 
     res.status(201).json({ name, price, user_id: req.user_id });
   }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
 import { routes } from "./routes";
+import { AppError } from "./utils/AppError";
 
 const PORT = 3333;
 
@@ -9,7 +10,10 @@ app.use(express.json());
 app.use(routes);
 
 app.use((err: any, req: Request, res: Response, _: any) => {
-  res.status(500).json({ message: "Erro ao tentar criar um produto!" });
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+  res.status(500).json({ message: err.message });
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
